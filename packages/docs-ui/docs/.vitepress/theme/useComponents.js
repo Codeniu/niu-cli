17e4e6bae5c976571e630a007023e9b458fe9a52
codeniu <codeniu@163.com>
2023-02-22 13:13:49 +0000

    🐞 fix: 文件名大小写问题

--- packages/docs-ui/docs/.vitepress/theme/useComponents.js
+++ packages/docs-ui/docs/.vitepress/theme/useComponents.js
@@ -1,16 +1,16 @@
-// import BasicTitle from '../../../src/BasicTitle/index.vue'
+import BasicTitle from '../../../src/BasicTitle/index.vue'
 import NDivider from '../../../src/Divider/index.vue'
 import NNav from '../../../src/Nav/index.vue'
 import NPicWrap from '../../../src/PicWrap/index.vue'
 import NComment from '../../../src/Comment/index.vue'
 import NRadioButton from '../../../src/RadioButtonGroup/demo.vue'
-import NSteps from '../../../src/steps/index.vue'
-import MileStone from '../../../src/steps/demo/index.vue'
+import NSteps from '../../../src/Steps/index.vue'
+import MileStone from '../../../src/Steps/demo/index.vue'
 
 import Wrap from './Wrap.vue'
 
 export function useComponents(app) {
-  // app.component('BasicTitle', BasicTitle)
+  app.component('BasicTitle', BasicTitle)
   app.component('NDivider', NDivider)
   app.component('NNav', NNav)
   app.component('NPicWrap', NPicWrap)
