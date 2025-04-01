4609dd76375e03a05e7068357500dabdac8fe4bd
codeniu <codeniu@163.com>
2025-04-01 11:36:18 +0800

    feat: 更新模板

--- packages/create-niu/template-vben1/src/App.vue
+++ packages/create-niu/template-vben1/src/App.vue
@@ -3,3 +3,18 @@
     <RouterView />
   </a-config-provider>
 </template>
+
+<script>
+  import zhCN from 'ant-design-vue/es/locale/zh_CN'
+  import dayjs from 'dayjs'
+  import 'dayjs/locale/zh-cn'
+  dayjs.locale('zh-cn')
+
+  export default {
+    data() {
+      return {
+        locale: zhCN,
+      }
+    },
+  }
+</script>
